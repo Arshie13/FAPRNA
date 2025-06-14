a01a157082abfa66972aa633f31b870e2ddb129f
Arshie13 <aaron.ciervo-20@cpu.edu.ph>
2025-06-14 21:52:36 +0800

    fix: a

--- src/app/admin/news/edit/[id]/page.tsx
+++ src/app/admin/news/edit/[id]/page.tsx
@@ -1,42 +1,33 @@
-// import type { Metadata } from "next"
+import type { Metadata } from "next"
 import NewsForm from "@/components/admin/NewsForm"
 import { getNewsById } from "@/lib/actions/news-actions"
 import { notFound } from "next/navigation"
 
-// type EditNewsPageProps = Promise<{
-//   params: {
-//     id: string
-//   }
-// }>
+type Props = Promise<{
+  id: string
+}>
 
-// export async function generateMetadata({ params }: EditNewsPageProps): Promise<Metadata> {
-//   const id = params.id
-//   const news = await getNewsById(id)
+export async function generateMetadata(props: { params: Props }): Promise<Metadata> {
+  const params = await props.params;
+  const id = await params.id
+  console.log(id)
+  const news = await getNewsById(id)
 
-//   if (!news) {
-//     return {
-//       title: "News Not Found | FAPRNA-NV Admin",
-//     }
-//   }
-//   if (!news) {
-//     return {
-//       title: "News Not Found | FAPRNA-NV Admin",
-//     }
-//   }
+  if (!news) {
+    return {
+      title: "News Not Found | FAPRNA-NV Admin",
+    }
+  }
 
-//   return {
-//     title: `Edit ${news.id} | FAPRNA-NV Admin`,
-//     description: `Edit news item: ${news.description}`,
-//   }
-// }
-//   return {
-//     title: `Edit ${news.id} | FAPRNA-NV Admin`,
-//     description: `Edit news item: ${news.description}`,
-//   }
-// }
+  return {
+    title: `Edit ${news.title} | FAPRNA-NV Admin`,
+    description: `Edit news item: ${news.description}`,
+  }
+}
 
-export default async function EditNewsPage(props: { params: { id: string } }) {
-  const { id } = await props.params
+export default async function EditNewsPage(props: { params: Props }) {
+  const params = await props.params;
+  const id = await params.id
   const news = await getNewsById(id)
 
   if (!news) {
